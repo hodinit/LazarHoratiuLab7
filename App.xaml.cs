15c044f27151a3de10412914b67c21d7867f7af7
HoratiuLazar <horatiulazar2003@gmail.com>
2024-12-07 11:14:31 +0200

    Laborator8 finalizat

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,7 +1,24 @@
-﻿namespace LazarHoratiuLab7
+﻿using LazarHoratiuLab7.Data;
+
+namespace LazarHoratiuLab7
 {
     public partial class App : Application
     {
+        static ShoppingListDatabase database;
+        public static ShoppingListDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new
+                    ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.
+                    LocalApplicationData), "ShoppingList.db3"));
+                }
+                return database;
+            }
+        }
+
         public App()
         {
             InitializeComponent();
