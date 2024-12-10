c23692c981a4bdf3eb45f4ed56ceaf75425dce9e
HoratiuLazar <horatiulazar2003@gmail.com>
2024-12-10 17:14:28 +0200

    Laborator 10 finalizat

--- Data/ShoppingListDatabase.cs
+++ Data/ShoppingListDatabase.cs
@@ -12,6 +12,7 @@ namespace LazarHoratiuLab7.Data
             _database.CreateTableAsync<ShopList>().Wait();
             _database.CreateTableAsync<Product>().Wait();
             _database.CreateTableAsync<ListProduct>().Wait();
+            _database.CreateTableAsync<Shop>().Wait();
         }
 
         public Task<int> SaveProductAsync(Product product)
@@ -79,6 +80,27 @@ namespace LazarHoratiuLab7.Data
             + " inner join ListProduct LP"
             + " on P.ID = LP.ProductID where LP.ShopListID = ?",
             shoplistid);
+        }
+
+        public Task<List<Shop>> GetShopsAsync()
+        {
+            return _database.Table<Shop>().ToListAsync();
+        }
+        public Task<int> SaveShopAsync(Shop shop)
+        {
+            if (shop.ID != 0)
+            {
+                return _database.UpdateAsync(shop);
+            }
+            else
+            {
+                return _database.InsertAsync(shop);
+            }
+        }
+
+        public Task<int> DeleteShopAsync(Shop shop)
+        {
+            return _database.DeleteAsync(shop);
         }
     }
 }
